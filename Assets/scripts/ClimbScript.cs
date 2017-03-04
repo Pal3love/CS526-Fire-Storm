a9a4bfb68a9719ddb7f1076e8484adf2873e2edb
z-ling <z-ling00@hotmail.com>
2017-03-03 16:24:46 -0800

    mmerge rope

--- Assets/scripts/ClimbScript.cs
+++ Assets/scripts/ClimbScript.cs
@@ -12,26 +12,30 @@ public class ClimbScript : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D otherCollider)
 	{
 
-//		Debug.Log("entered ");
+		Debug.Log("entered ");
 		// Is this player?
 		player = otherCollider.gameObject.GetComponent<PlayerScript>();
 
 		if (player != null)
 		{ 
 			originalGravityScale = player.GetComponent<Rigidbody2D>().gravityScale;
-			player.GetComponent<Rigidbody2D>().gravityScale = 0;
+			player.GetComponent<Rigidbody2D> ().gravityScale = 0;
 		}
 	}
 	void OnTriggerStay2D(Collider2D otherCollider)
 	{
-//		Debug.Log("stay function");
+		Debug.Log("stay function");
+
 		if (player != null)
 		{ 
-			if (Input.GetKey (KeyCode.W)) { // used a tag to ID collider as player
-				player.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 1);
+			if (Input.GetKey (KeyCode.UpArrow)) { // used a tag to ID collider as player
+				player.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0,  1);
 
-			} else if (Input.GetKey(KeyCode.S)) {
+			} else if (Input.GetKey (KeyCode.DownArrow)) {
 				player.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, -1);
+
+			} else {
+				player.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
 			}
 		}
 	}
@@ -39,13 +43,11 @@ public class ClimbScript : MonoBehaviour {
 	void OnTriggerExit2D(Collider2D otherCollider)
 	{
 
-//		Debug.Log("tigger exit");
-		// Is this player?
-		PlayerScript player = otherCollider.gameObject.GetComponent<PlayerScript>();
+		Debug.Log("tigger exit");
 
 		if (player != null)
 		{ 
-			player.GetComponent<Rigidbody2D> ().gravityScale = originalGravityScale;
+			player.GetComponent<Rigidbody2D> ().gravityScale = 1;
 
 		}
 	}
