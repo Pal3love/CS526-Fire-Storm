1b50967cde927d79f32d34b5d90b4ed7732a439e
NeverWorld <chenhuig@usc.edu>
2017-04-05 00:57:41 -0700

    fix jump bug

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -16,7 +16,6 @@ public class PlayerScript : MonoBehaviour {
 	public float jumpSpeed;
 	public Vector2 jumpVector;
 	public bool isGrounded;
-	public Transform grounder;
 	public float radiuss;
 	public LayerMask ground;
     public Rigidbody2D bulletPrefab1;
@@ -63,9 +62,9 @@ public class PlayerScript : MonoBehaviour {
 			
 
 		// 5 - Jumping
-		isGrounded = Physics2D.OverlapCircle(grounder.transform.position,radiuss,ground);
 		if(Input.GetKey(KeyCode.UpArrow) && isGrounded){
 			rigidbodyComponent.AddForce (jumpVector, ForceMode2D.Force);
+            isGrounded = false;
 		}
 
 
@@ -109,14 +108,8 @@ public class PlayerScript : MonoBehaviour {
 //		rigidbodyComponent.velocity = movement;
 	}
 
-	void OnDrawGizmos(){
-		Gizmos.color = Color.white;
-		Gizmos.DrawWireSphere (grounder.transform.position, radiuss);
-	}
-
 	void OnTriggerEnter2D(Collider2D otherCollider)
 	{
-		Debug.Log ("!!!!!!!");
 		if (otherCollider.tag == "Rope") {
 			if (!isClimbing) {
 				isClimbing = true;
@@ -126,12 +119,16 @@ public class PlayerScript : MonoBehaviour {
 
 	void OnTriggerExit2D(Collider2D otherCollider)
 	{
-		Debug.Log ("~~~~~~~~~");
 		if (otherCollider.tag == "Rope") {
 			if (isClimbing) {
 				isClimbing = false;
 			}
 		}
 	}
-				
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
 }
