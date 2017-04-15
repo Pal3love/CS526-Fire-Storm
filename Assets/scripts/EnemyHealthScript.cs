15ce88628d5a6fa7b0202dde22f1b26fb1f34b5e
z-ling <z-ling00@hotmail.com>
2017-04-15 16:12:29 -0700

    add pill

--- Assets/scripts/EnemyHealthScript.cs
+++ Assets/scripts/EnemyHealthScript.cs
@@ -5,18 +5,16 @@ using UnityEngine.UI;
 
 public class EnemyHealthScript : MonoBehaviour {
 
-    public float enemyHP = 10;
+    public float enemyHP = 3;
     public float currentHP;
+    public float Atk = 1;
     public Slider healthBar;
-	private long enemyScore;  // Added by Shiyu He
 
-	private PlayerScript PlayerS;
+	public GameObject Pill;
     
 	void Start () {
         currentHP = enemyHP;
         healthBar.value = caculateHealth();
-		PlayerS = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<PlayerScript> ();
-		enemyScore = 0;
     }
 	
 	// Update is called once per frame
@@ -26,17 +24,25 @@ public class EnemyHealthScript : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "PlayerShot")
-		{
-           currentHP -= PlayerS.Atk; //生命值-10
-			PlayerS.playerScore += PlayerS.Atk * 2;  //中一枪加小分
+        {
+           currentHP -= Atk; //生命值-10
 
             healthBar.value = currentHP/enemyHP;
+
             if (currentHP <= 0)
             {
-				PlayerS.playerScore += 50;  //敌人挂掉加大分
                 Destroy(gameObject); //摧毀物件
+//				Debug.Log("enemy died");
+
+				int randomNumber = Random.Range(0, 100);
+				if (randomNumber >= 50) {
+					Instantiate (Pill, new Vector2 (col.transform.position.x, col.transform.position.y + 1), Quaternion.identity);
+				}
+//				Instantiate (Pill, new Vector2 (col.transform.position.x, col.transform.position.y ), Quaternion.identity);
+
 
             }
+
             Destroy(col.gameObject);
         }
 
--- Assets/scripts/PickUpScript.cs
+++ Assets/scripts/PickUpScript.cs
@@ -5,23 +5,42 @@ using UnityEngine;
 public class PickUpScript : MonoBehaviour {
 
 	PlayerScript player;
+	PlayerHealthScript playerAttribute;
+
+	int pillVaule = 5;
 
 	void OnTriggerEnter2D(Collider2D otherCollider)
 	{
+
 		Debug.Log("entered pick up");
 		// Is this player?
 		player = otherCollider.gameObject.GetComponent<PlayerScript>();
+		playerAttribute = otherCollider.gameObject.GetComponent<PlayerHealthScript>();
+//		recover = 
 
         if (player != null)
 		{ 
 			Destroy(gameObject, 0);
-			player.Atk = player.Atk + 5;
+
+			//if picking up orange
+			if (playerAttribute != null)
+            {
+				playerAttribute.enemyAtk++;  //increase attack
+            }//add effect
+
+			//if picking up pill
+			if (this.gameObject.CompareTag ("Pill"))
+			{
+				Debug.Log("before: " + playerAttribute.life);
+				playerAttribute.life += pillVaule;
+				Debug.Log("after: " + playerAttribute.life);
+			}
+
 		}
 	}
 		
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
